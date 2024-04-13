f1e2262dd17c55649bfa9757ec1a4238e191b954
bakr <bakr0592260950@gmail.com>
2024-04-13 18:56:49 +0200

    .

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.assignment1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -17,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
     CheckBox level1;
     CheckBox level2;
     CheckBox level3;
-    Button submit;
 
+    CheckBox rem;
+
+    Button submit;
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+
+    public static final String FLAG = "FLAG";
+    boolean flag = false;
+    int lvl ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         level2 = findViewById(R.id.level2);
         level3 = findViewById(R.id.level3);
         submit = findViewById(R.id.submit);
+        rem    = findViewById(R.id.rem);
+        setupSharedPrefs();
+        checkPrefs();
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +50,13 @@ public class MainActivity extends AppCompatActivity {
                     if (level2.isChecked()||level3.isChecked()){
                         Toast.makeText(MainActivity.this,"Please Select only One !",Toast.LENGTH_LONG).show();
                         return;
+                    }
+                    if (rem.isChecked()){
+                        if (!flag){
+                            editor.putInt("lvl",1);
+                            editor.putBoolean(FLAG,true);
+                            editor.commit();
+                        }
                     }
                     Intent intent = new Intent(MainActivity.this,SecActivity.class);
                     intent.putExtra("level","1");
@@ -46,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this,"Please Select only One !",Toast.LENGTH_LONG).show();
                         return;
                     }
+                    if (rem.isChecked()){
+                        if (!flag){
+                            editor.putInt("lvl",2);
+                            editor.putBoolean(FLAG,true);
+                            editor.commit();
+                        }
+                    }
                     Intent intent = new Intent(MainActivity.this,SecActivity.class);
                     intent.putExtra("level","2");
                     startActivity(intent);
@@ -55,6 +83,13 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this,"Please Select only One !",Toast.LENGTH_LONG).show();
                         return;
                     }
+                    if (rem.isChecked()){
+                        if (!flag){
+                            editor.putInt("lvl",3);
+                            editor.putBoolean(FLAG,true);
+                            editor.commit();
+                        }
+                    }
                     Intent intent = new Intent(MainActivity.this,SecActivity.class);
                     intent.putExtra("level","3");
                     startActivity(intent);
@@ -62,5 +97,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void setupSharedPrefs() {
+        prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        editor = prefs.edit();
+    }
+    private void checkPrefs() {
+        flag = prefs.getBoolean(FLAG, false);
 
+        if(flag){
+                lvl = prefs.getInt("lvl",0);
+                switch (lvl){
+                    case 1:
+                        level1.setChecked(true);
+                        break;
+                    case 2:
+                        level2.setChecked(true);
+                        break;
+                    case 3:
+                        level3.setChecked(true);
+                        break;
+                }
+                editor.clear();
+                editor.commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkPrefs();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPrefs();
+    }
 }
